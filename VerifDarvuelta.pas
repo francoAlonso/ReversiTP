c70f7cfcc86683782d0d6168b98b9bc91ed08f2a
Handerllon <manuel121097@gmail.com>
2017-04-23 21:33:39 -0300

    Update VerifDarvuelta.pas

--- VerifDarvuelta.pas
+++ VerifDarvuelta.pas
@@ -1,22 +1,23 @@
-function VerificarValido (tablero:tTablero;         //beta :)
-  posicionX:byte; posicionY:byte):boolean;
-var Valido: boolean;
-  begin  Valido:=false
+function VerificarValido (tablero:tTablero;posicionX:byte; posicionY:byte):boolean; //beta :)
+  var Valido: boolean;
+  begin  
+    Valido:=false
     if tablero[posicionX+1,posicionY]=FichaContraria then
-     begin
-      while (tablero[posicionX+1,posicionY]<>FichaJugadorActual) and (tablero[posicionX+1,posicionY]<>'_') do
-        begin
-          if (tablero[posicionX+1,posicionY]=FichaContraria) then Valido:=true
-          else posicionX:=posicionX+1;
-        end;
-   end;
+      begin
+        while (tablero[posicionX+1,posicionY]<>FichaJugadorActual) and (tablero[posicionX+1,posicionY]<>'_') do
+          begin
+            if (tablero[posicionX+1,posicionY]=FichaContraria) then 
+              Valido:=true
+            else posicionX:=posicionX+1;
+          end;
+      end;
     VerificarValido:=Valido;
- end;   
+end;   
     
     
     
-    procedure DarVuelta (var tablero:tTablero; VerificarValido:boolean);
+procedure DarVuelta (var tablero:tTablero; VerificarValido:boolean);
 var
   begin
   if (VerificarValido:=true) then
-  FichaContraria:=FichaJugadorActual; 
+  FichaContraria:=FichaJugadorActual;
